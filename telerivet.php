ff4360ad93557bdf3a289b6e80c49b1eb97aee91
Michael McAndrew <michaelmcandrew@thirdsectordesign.org>
2017-01-26 00:39:56 +0000

    adding base for SMS extension, inspired by twilio integration

---
+++ telerivet.php
@@ -0,0 +1,144 @@
+<?php
+
+require_once 'telerivet.civix.php';
+
+/**
+ * Implements hook_civicrm_config().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_config
+ */
+function telerivet_civicrm_config(&$config) {
+  _telerivet_civix_civicrm_config($config);
+}
+
+/**
+ * Implements hook_civicrm_xmlMenu().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
+ */
+function telerivet_civicrm_xmlMenu(&$files) {
+  _telerivet_civix_civicrm_xmlMenu($files);
+}
+
+/**
+ * Implements hook_civicrm_install().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
+ */
+function telerivet_civicrm_install() {
+  $groupID = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionGroup','sms_provider_name','id','name');
+  $params  =
+    array('option_group_id' => $groupID,
+          'label' => 'Telerivet',
+          'value' => 'org.ndi.sms.telerivet',
+          'name'  => 'telerivet',
+          'is_default' => 1,
+          'is_active'  => 1,
+          'version'    => 3,);
+  require_once 'api/api.php';
+  civicrm_api( 'option_value','create', $params );
+
+ _telerivet_civix_civicrm_install();
+}
+
+/**
+ * Implements hook_civicrm_postInstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
+ */
+function telerivet_civicrm_postInstall() {
+  _telerivet_civix_civicrm_postInstall();
+}
+
+/**
+ * Implements hook_civicrm_uninstall().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
+ */
+function telerivet_civicrm_uninstall() {
+  $optionID = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionValue','telerivet','id','name');
+  if ($optionID)
+    CRM_Core_BAO_OptionValue::del($optionID);
+
+  $filter    =  array('name'  => 'org.ndi.sms.telerivet');
+  $Providers =  CRM_SMS_BAO_Provider::getProviders(False, $filter, False);
+  if ($Providers){
+    foreach($Providers as $key => $value){
+      CRM_SMS_BAO_Provider::del($value['id']);
+    }
+  }
+  _telerivet_civix_civicrm_uninstall();
+}
+
+/**
+ * Implements hook_civicrm_enable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
+ */
+function telerivet_civicrm_enable() {
+  $optionID = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionValue','telerivet' ,'id','name');
+  if ($optionID)
+    CRM_Core_BAO_OptionValue::setIsActive($optionID, TRUE);
+
+  $filter    =  array('name' => 'org.ndi.sms.telerivet');
+  $Providers =  CRM_SMS_BAO_Provider::getProviders(False, $filter, False);
+  if ($Providers){
+    foreach($Providers as $key => $value){
+      CRM_SMS_BAO_Provider::setIsActive($value['id'], TRUE);
+    }
+  }
+
+  _telerivet_civix_civicrm_enable();
+}
+
+/**
+ * Implements hook_civicrm_disable().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
+ */
+function telerivet_civicrm_disable() {
+  $optionID = CRM_Core_DAO::getFieldValue('CRM_Core_DAO_OptionValue','telerivet','id','name');
+  if ($optionID)
+    CRM_Core_BAO_OptionValue::setIsActive($optionID, FALSE);
+
+  $filter    =  array('name' =>  'org.ndi.sms.telerivet');
+  $Providers =  CRM_SMS_BAO_Provider::getProviders(False, $filter, False);
+  if ($Providers){
+    foreach($Providers as $key => $value){
+      CRM_SMS_BAO_Provider::setIsActive($value['id'], FALSE);
+    }
+  }
+
+  _telerivet_civix_civicrm_disable();
+}
+
+/**
+ * Implements hook_civicrm_upgrade().
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
+ */
+function telerivet_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
+  return _telerivet_civix_civicrm_upgrade($op, $queue);
+}
+
+/**
+ * Implements hook_civicrm_managed().
+ *
+ * Generate a list of entities to create/deactivate/delete when this module
+ * is installed, disabled, uninstalled.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
+ */
+function telerivet_civicrm_managed(&$entities) {
+  _telerivet_civix_civicrm_managed($entities);
+}
+
+/**
+ * Implements hook_civicrm_caseTypes().
+ *
+ * Generate a list of case-types.
+ *
+ * Note: This hook only runs in CiviCRM 4.4+.
+ *
+ * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
+ */
